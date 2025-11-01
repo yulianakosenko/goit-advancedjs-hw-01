6eaea6c31e7235f062629c8cf3946b00f6eb1bda
YULIYA_KOSTENKO <yuliashkodina1@gmail.com>
2025-11-01 20:15:40 +0100

    fix: multi-page build (gallery + form)

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
+// vite.config.js
 import { defineConfig } from 'vite';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: 'public', // HTML у public
-  base: '',
+  root: 'public',
   build: {
     outDir: '../dist',
     emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'public/index.html'),
+        gallery: resolve(__dirname, 'public/1-gallery.html'),
+        form: resolve(__dirname, 'public/2-form.html'),
+      },
+    },
   },
 });
