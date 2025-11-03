3f7a30ff657e906134d992e2e0f657322d529220
YULIYA_KOSTENKO <yuliashkodina1@gmail.com>
2025-11-03 20:36:21 +0100

    11 03.11.2025

--- vite.config.js
+++ vite.config.js
@@ -6,17 +6,16 @@ import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
-  root: 'src/public',
+  root: 'src',
   base: '/goit-advancedjs-hw-01/',
-
   build: {
-    outDir: '../../dist',
+    outDir: '../dist',
     emptyOutDir: true,
     rollupOptions: {
       input: {
-        main: resolve(__dirname, 'src/public/index.html'),
-        gallery: resolve(__dirname, 'src/public/1-gallery.html'),
-        form: resolve(__dirname, 'src/public/2-form.html'),
+        main: resolve(__dirname, 'src/index.html'),
+        gallery: resolve(__dirname, 'src/1-gallery.html'),
+        form: resolve(__dirname, 'src/2-form.html'),
       },
     },
   },
